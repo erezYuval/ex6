8f724ee2a78c620251b7bb1fa2b0b4d12681e597
yuvalavrami <yuval.avrami@gmail.com>
2015-06-14 15:35:13 +0300

    var exceptions skeleton

---
+++ src/oop/ex6/variables/exceptions/AttemptedChangeFinalVarException.java
@@ -0,0 +1,7 @@
+package oop.ex6.variables.exceptions;
+
+/**
+ * Created by yuvalavrami on 6/14/15.
+ */
+public class AttemptedChangeFinalVarException extends FinalVariableException{
+}
---
+++ src/oop/ex6/variables/exceptions/DeclaredFinalIntWithoutInitializationException.java
@@ -0,0 +1,9 @@
+package oop.ex6.variables.exceptions;
+
+import oop.ex6.variables.exceptions.FinalVariableException;
+
+/**
+ * Created by yuvalavrami on 6/14/15.
+ */
+public class DeclaredFinalIntWithoutInitializationException extends FinalVariableException{
+}
