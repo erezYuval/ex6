95ca6e52b28b1084be63be9e46a6ac9ca8d345de
Erez Levanon <erez.leavnon@mail.huji.ac.il>
2015-06-15 13:25:45 +0300

    methods done

--- src/oop/ex6/methods/Method.java
+++ src/oop/ex6/methods/Method.java
@@ -1,23 +1,41 @@
 package oop.ex6.methods;
 
 import oop.ex6.variables.VARIABLE_TYPES;
+import oop.ex6.variables.Variable;
+import oop.ex6.variables.VariableFactory;
+
+import java.util.ArrayList;
 
 /**
  * Created by Erez Levanon on 13/06/2015.
  */
 public class Method{
 
-    private VARIABLE_TYPES[] argumentsInOrder;
+    private ArrayList<Variable> variables;
+    private String name;
 
-    public Method(String methodName, VARIABLE_TYPES[] argumentsInOrder) {
-        //TODO constructor
+    public Method(String methodName, VARIABLE_TYPES[] argumentTypesInOrder, String[] argumentNamesInOrder ) {
+        variables = new ArrayList<>();
+        name = methodName;
+        for (int i = 0; i < argumentTypesInOrder.length ; i++) {
+            Variable variable = VariableFactory.produceVariable(argumentTypesInOrder[i], argumentNamesInOrder[i]);
+            variable.setInitialized();
+            variables.add(variable);
+        }
     }
 
-    public VARIABLE_TYPES[] getArgumentTypes() {
-        return argumentsInOrder;
+    public void checkArgumentInIndex(int index, String value) {
+        // TODO catch illegal value
+        variables.get(index).setValue(value);
     }
 
-    public void attemptCallMethod(String[] values) {
+    public void checkArgumentInIndex(int index, Variable variable) {
+        // TODO catch illegal value
+        variables.get(index).setValue(variable);
+    }
+
+    public String getName() {
+        return name;
     }
 
 }
