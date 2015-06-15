54e9257ca89a54ebf9d77d0fac9d95934916a23a
yuvalavrami <yuval.avrami@gmail.com>
2015-06-15 15:40:27 +0300

    added "LegalLineParser" to parser package

---
+++ src/oop/ex6/parser/LegalLineParser.java
@@ -0,0 +1,10 @@
+package oop.ex6.parser;
+
+/**
+ * Created by yuvalavrami on 6/15/15.
+ *
+ * receives legal lines from the parser (excluding illegal, empty and comment lines)
+ * and extracts methods and variables from them
+ */
+public class LegalLineParser {
+}
