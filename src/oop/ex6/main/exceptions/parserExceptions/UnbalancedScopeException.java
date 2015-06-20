ad959ab4845705b8b7e1d0259a6eab5fbe8ae00b
yuvalavrami <yuval.avrami@gmail.com>
2015-06-20 14:25:42 +0300

    fixes Unbalanced name

--- src/oop/ex6/main/exceptions/parserExceptions/UnbalancedScopeException.java
+++ src/oop/ex6/main/exceptions/parserExceptions/UnbalancedScopeException.java
@@ -5,6 +5,6 @@ import oop.ex6.main.exceptions.SjavaException;
 /**
  * Created by yuvalavrami on 6/14/15.
  */
-public class unbalancedScopeException extends SjavaException{
+public class UnbalancedScopeException extends SjavaException{
     protected final static String ERROR_MESSAGE = "could not parse file - mismatched number of opening and closing brackets";
 }
