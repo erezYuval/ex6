ca8e2f4110d7ec875b62f5757a856bca567dab0c
yuvalavrami <yuval.avrami@gmail.com>
2015-06-20 11:17:49 +0300

    legalLineParser

--- src/oop/ex6/parser/LegalLineParser.java
+++ src/oop/ex6/parser/LegalLineParser.java
@@ -2,6 +2,8 @@ package oop.ex6.parser;
 
 import oop.ex6.scopes.Scope;
 
+import java.util.ArrayList;
+
 /**
  * Created by yuvalavrami on 6/15/15.
  *
@@ -11,33 +13,38 @@ import oop.ex6.scopes.Scope;
 public class LegalLineParser {
 
     static void parseLine(String line, int lineNumber, Scope currentScope){
-//        if(line.matches(JavaSPatterns.METHOD_SIGNATURE)){
-//            String[] methodNameHelper = line.split("\\(")[0].split(" ");
-//            String methodName = methodNameHelper[methodNameHelper.length - 1];
-//
-//
+        if(line.matches(JavaSPatterns.METHOD_SIGNATURE)){
+            String[] methodNameHelper = line.split("\\(")[0].split(" ");
+            String methodName = methodNameHelper[methodNameHelper.length - 1];
+
+
 //            Method newMethod = new Method(methodName, --vartypes--, --argtypes--, lineNumber);
-//        }
+        }
 
     }
 
     public static void main(String[] args) {
         String line = "void shits     (String asd, boolean true, int a) {";
 
+        ArrayList<String> typesInOrder = new ArrayList<>();
+        ArrayList<String> namesInOrder = new ArrayList<>();
+
         String[] arguments = line.split("\\(")[1].split(",");
-        for(String arg: arguments){
-            String[] splitArg = arg.split("\\s")
-            String argtype = splitArg[0];
-            System.out.println("type=" + argtype);
-            String argName = splitArg[1];
-            System.out.println("name=" + argName);
+        for (String arg : arguments) {
+            String argTrimmed = arg.trim();
+            String[] splitArg = argTrimmed.split("\\s");
+            typesInOrder.add(splitArg[0]);
+            namesInOrder.add(splitArg[1].split("\\)")[0]);
+        }
+        for(String name:namesInOrder){
+            System.out.println(name);
         }
 
-
-
-        }
-
+    }
 
 
     }
+
+
+
 
