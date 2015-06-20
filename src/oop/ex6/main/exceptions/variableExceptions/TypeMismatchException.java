73a79e80b5263429c86610b7925d266a145ca697
Erez Levanon <erez.leavnon@mail.huji.ac.il>
2015-06-20 14:18:57 +0300

    type mismatch

--- src/oop/ex6/main/exceptions/variableExceptions/TypeMismatchException.java
+++ src/oop/ex6/main/exceptions/variableExceptions/TypeMismatchException.java
@@ -3,9 +3,6 @@ package oop.ex6.main.exceptions.variableExceptions;
 import oop.ex6.main.exceptions.SjavaException;
 import oop.ex6.variables.Variable;
 
-/**
- * Created by yuvalavrami on 6/14/15.
- */
 public class TypeMismatchException extends VariableException {
     protected final String ERROR_MESSAGE = "cannot assign to variable - assigned type does not match required type";
     private String uniqueMessage;
