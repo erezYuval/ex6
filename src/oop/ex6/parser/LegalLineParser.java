d20b12a0dfebe69b24cc61b78b00d2a344c8e1d3
yuvalavrami <yuval.avrami@gmail.com>
2015-06-20 13:03:34 +0300

    legalLineParser: Method declaration

--- src/oop/ex6/parser/LegalLineParser.java
+++ src/oop/ex6/parser/LegalLineParser.java
@@ -1,6 +1,9 @@
 package oop.ex6.parser;
 
+import oop.ex6.methods.Method;
 import oop.ex6.scopes.Scope;
+import oop.ex6.variables.VARIABLE_TYPES;
+import oop.ex6.variables.VariableUtils;
 
 import java.util.ArrayList;
 
@@ -13,33 +16,114 @@ import java.util.ArrayList;
 public class LegalLineParser {
 
     static void parseLine(String line, int lineNumber, Scope currentScope){
-        if(line.matches(JavaSPatterns.METHOD_SIGNATURE)){
+        if(line.matches(JavaSPatterns.METHOD_SIGNATURE)){ //current line is method declaration
+
+            //find method name
             String[] methodNameHelper = line.split("\\(")[0].split(" ");
             String methodName = methodNameHelper[methodNameHelper.length - 1];
 
+            VARIABLE_TYPES[] varTypeArray = new VARIABLE_TYPES[0];
+            String[] namesArray = new String[0];
 
-//            Method newMethod = new Method(methodName, --vartypes--, --argtypes--, lineNumber);
+            //if there are no arguments leave name and type arrays blank
+            if(line.matches(".*\\(\\s*\\).*")){
+            } //do nothing
+
+            else { //there is at least one argument
+                //find methods arguments' types and names
+                ArrayList<String> typesInOrderAsStrings = new ArrayList<>();
+                ArrayList<VARIABLE_TYPES> typesInOrder = new ArrayList<>();
+
+                ArrayList<String> namesInOrder = new ArrayList<>();
+
+                String[] arguments = line.split("\\(")[1].split(",");
+                for (String arg : arguments) { //for each separate argument - separate between type and name
+                    String argTrimmed = arg.trim();
+                    String[] splitArg = argTrimmed.split("\\s+");
+                    typesInOrderAsStrings.add(splitArg[0]); // add type to type array
+                    namesInOrder.add(splitArg[1].split("\\)")[0]); // add name to name array
+                }
+
+                for (String type : typesInOrderAsStrings) { // convert strings to variableType objects
+                    typesInOrder.add(VariableUtils.stringToType(type));
+                }
+                varTypeArray = new VARIABLE_TYPES[typesInOrder.size()];
+                varTypeArray = typesInOrder.toArray(varTypeArray); //convert arrayList to array
+
+                namesArray = new String[namesInOrder.size()];
+                namesArray = namesInOrder.toArray(namesArray); //convert arrayList to array
+            }
+
+            //create new method object based on extracted arguments
+            Method newMethod = new Method(methodName, varTypeArray, namesArray, lineNumber);
         }
 
     }
 
+    private static void dealWithReturnStatement(String line, Scope scope) {
+
+        scope.getParentMethod()
+        scope.getParentMethod().setHasReturnStatement(true);
+
+
     public static void main(String[] args) {
-        String line = "void shits     (String asd, boolean true, int a) {";
+        int lineNumber = 1;
+        String line = "  void  shits     ( int a, boolean true, String aaa, char a) {";
 
-        ArrayList<String> typesInOrder = new ArrayList<>();
-        ArrayList<String> namesInOrder = new ArrayList<>();
 
-        String[] arguments = line.split("\\(")[1].split(",");
-        for (String arg : arguments) {
-            String argTrimmed = arg.trim();
-            String[] splitArg = argTrimmed.split("\\s");
-            typesInOrder.add(splitArg[0]);
-            namesInOrder.add(splitArg[1].split("\\)")[0]);
+        String[] methodNameHelper = line.split("\\(")[0].split(" ");
+        String methodName = methodNameHelper[methodNameHelper.length - 1];
+
+        VARIABLE_TYPES[] varTypeArray = new VARIABLE_TYPES[0];
+        String[] namesArray = new String[0];
+
+        //if there are no arguments leave name and type arrays blank
+        if(line.matches(".*\\(\\s*\\).*")){
+            System.out.println("there are no arguments");
         }
-        for(String name:namesInOrder){
+
+        else { //there is at least one argument
+            //find methods arguments' types and names
+            ArrayList<String> typesInOrderAsStrings = new ArrayList<>();
+            ArrayList<VARIABLE_TYPES> typesInOrder = new ArrayList<>();
+
+            ArrayList<String> namesInOrder = new ArrayList<>();
+
+            String[] arguments = line.split("\\(")[1].split(",");
+            for (String arg : arguments) { //for each separate argument - separate between type and name
+                String argTrimmed = arg.trim();
+                String[] splitArg = argTrimmed.split("\\s+");
+                typesInOrderAsStrings.add(splitArg[0]); // add type to type array
+                namesInOrder.add(splitArg[1].split("\\)")[0]); // add name to name array
+            }
+
+            for (String type : typesInOrderAsStrings) { // convert strings to variable type objects
+                typesInOrder.add(VariableUtils.stringToType(type));
+            }
+            varTypeArray = new VARIABLE_TYPES[typesInOrder.size()];
+            varTypeArray = typesInOrder.toArray(varTypeArray); //convert arraylist to array
+
+
+            namesArray = new String[namesInOrder.size()];
+            namesArray = namesInOrder.toArray(namesArray); //convert arraylist to array
+        }
+
+        System.out.println("methodname = " + methodName);
+        System.out.println("---");
+        System.out.println("types in order:");
+        for (VARIABLE_TYPES type: varTypeArray){
+            System.out.println(type);
+    }
+        System.out.println("---");
+        System.out.println("names in order:");
+        for (String name: namesArray){
             System.out.println(name);
         }
+        Method newMethod = new Method(methodName, varTypeArray, namesArray, lineNumber);
 
+        System.out.println("~~~~~");
+        System.out.println("methodname = " + newMethod.getName());
+        System.out.println(newMethod.getVariables());
     }
 
 
