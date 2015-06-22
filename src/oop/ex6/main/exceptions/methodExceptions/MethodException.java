155c46faf0cbb957b6c2787a65f64b0a34a064f6
yuvalavrami <yuval.avrami@gmail.com>
2015-06-22 14:33:43 +0300

    added documentation to scope exceptions

--- src/oop/ex6/main/exceptions/methodExceptions/MethodException.java
+++ src/oop/ex6/main/exceptions/methodExceptions/MethodException.java
@@ -3,8 +3,6 @@ package oop.ex6.main.exceptions.methodExceptions;
 import oop.ex6.main.exceptions.SjavaException;
 
 /**
- * Created by Erez Levanon on 20/06/2015.
- *
  * parent class for method exceptions.
  */
 public class MethodException extends SjavaException {
