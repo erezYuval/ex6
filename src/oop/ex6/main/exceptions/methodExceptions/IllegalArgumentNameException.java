2b0a94e6c6761a4fdba6d5970ea2125ce7bc04f4
yuvalavrami <yuval.avrami@gmail.com>
2015-06-22 14:02:25 +0300

    added documentation to variableUtils

--- src/oop/ex6/main/exceptions/methodExceptions/IllegalArgumentNameException.java
+++ src/oop/ex6/main/exceptions/methodExceptions/IllegalArgumentNameException.java
@@ -1,13 +1,11 @@
 package oop.ex6.main.exceptions.methodExceptions;
 
-import oop.ex6.main.exceptions.SjavaException;
-import oop.ex6.main.exceptions.variableExceptions.IllegalNameException;
-
 /**
  * Created by yuvalavrami on 6/14/15.
  */
 public class IllegalArgumentNameException extends MethodException{
     private final static String TYPE_ERROR_MESSAGE = "cannot use argument with this name: name is illegal";
+
 
     public IllegalArgumentNameException(String name) {
         ERROR_MESSAGE = TYPE_ERROR_MESSAGE;
