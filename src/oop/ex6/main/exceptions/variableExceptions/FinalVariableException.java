5062a1d5749f92da3a12dc55d46f240ec1152bf5
yuvalavrami <yuval.avrami@gmail.com>
2015-06-22 11:32:42 +0300

    added documentation comments to variable exceptions

--- src/oop/ex6/main/exceptions/variableExceptions/FinalVariableException.java
+++ src/oop/ex6/main/exceptions/variableExceptions/FinalVariableException.java
@@ -1,11 +1,9 @@
 package oop.ex6.main.exceptions.variableExceptions;
 
-import oop.ex6.main.exceptions.SjavaException;
-
 /**
  * Created by yuvalavrami on 6/14/15.
  *
- * parent class for methodExceptions dealing with variables declared as final
+ * parent class for variableExceptions dealing with variables declared as final
  */
 public class FinalVariableException extends VariableException {
 }
--- src/oop/ex6/main/exceptions/variableExceptions/VariableException.java
+++ src/oop/ex6/main/exceptions/variableExceptions/VariableException.java
@@ -1,10 +1,11 @@
 package oop.ex6.main.exceptions.variableExceptions;
 
 import oop.ex6.main.exceptions.SjavaException;
-import oop.ex6.variables.Variable;
 
 /**
  * Created by Erez Levanon on 20/06/2015.
+ *
+ * parent class for all variable exceptions
  */
 public abstract class VariableException extends SjavaException {
 
