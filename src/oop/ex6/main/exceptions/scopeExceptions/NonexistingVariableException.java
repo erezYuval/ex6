f060daf7e21211982d2d54be8e0bb09487e693af
yuvalavrami <yuval.avrami@gmail.com>
2015-06-22 14:12:17 +0300

    added documentation to variable exceptions

--- src/oop/ex6/main/exceptions/scopeExceptions/NonexistingVariableException.java
+++ src/oop/ex6/main/exceptions/scopeExceptions/NonexistingVariableException.java
@@ -1,14 +1,15 @@
 package oop.ex6.main.exceptions.scopeExceptions;
 
-import oop.ex6.main.exceptions.SjavaException;
-import oop.ex6.variables.Variable;
-
 /**
  * Created by yuvalavrami on 6/14/15.
  */
 public class NonexistingVariableException extends ScopeException {
    private final static String TYPE_ERROR_MESSAGE = "variable does not exist";
 
+   /**
+    * constructor for exception. holds an error message according to the type of exception,
+    * and a unique message updated according to the non-existing variable that was called for which it was thrown.
+    */
    public NonexistingVariableException(String variableName) {
       ERROR_MESSAGE = TYPE_ERROR_MESSAGE;
       uniqueMessage = "the variable -" + variableName + "- doesn't exist.";
